d3e608a24ed992d32b71cb5048acbd3c40a2b37d
Noah Cowan <nsncowan@gmail.com>
2023-01-31 20:50:11 -0800

    WIP Co-authored by: Geoffrey Gao <geoffreygao1@gmail.com>

--- AllergyScore.Tests/ModelTests/AllergyTests.cs
+++ AllergyScore.Tests/ModelTests/AllergyTests.cs
@@ -1,3 +1,22 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TriangleTracker.Models;
+using AllergyScore;
+
+namespace AllergyScore.Models
+{
+  [TestClass]
+  public class ScoreTests
+  {
+//     [TestMethod]
+// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+// {
+//   // any necessary logic to prep for test; instantiating new classes, etc.
+//   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    [TestMethod]
+    public void ScoreCard_CreateScoreCard_True()
+    {
+      Score test = new Score("1");
+      Assert.AreEqual(1, test.UserInputScore);
+    }
+}
+}
 
